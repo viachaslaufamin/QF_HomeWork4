3cbde8acf82e0ebf56d2247545b816c7a381301f
viachaslaufamin <119659118+viachaslaufamin@users.noreply.github.com>
2023-08-15 15:54:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+//let d = 7;
+//console.log(d);
+//console.log(d**2);
+//console.log(d**3);
+
+
+
+//let zad2 = prompt('Enter the number');
+//if (zad2 > 0) {
+//    let newzad2 = zad2 * 2;
+//   console.log("Новое значение:", newzad2);
+//}
+//else {
+//   console.log("Число не положительное.");
+//};
+
+
+
+//let zad3a = Number(prompt('Enter the number1'));
+//let zad3b = Number(prompt('Enter the number2'));
+//if (zad3a > zad3b) {
+//   let rezultat1 = Number(zad3a + zad3b);
+//   console.log(rezultat1);
+//}
+//else if (zad3a<zad3b) {
+//   let rezultat2 = Number(zad3a * zad3b);
+//   console.log(rezultat2);
+//}
+//else {
+//   console.log('числа одинаковые');
+//};
+
+
+
+//let zad4 = prompt('Enter the number');
+//if (zad4<0){
+//   let rezultat1 = zad4 ** 2;
+//   console.log(rezultat1);
+//}
+//else {
+//   alert('Ошибка')
+//}
+
+
+
+//let zad5a = Number(prompt('введите количества учеников'));
+//let zad5b = Number(prompt('введите количества стульев'));
+//if (zad5a<zad5b || zad5a==zad5b){
+//   alert('стульев хватает');
+//}
+//else{
+//   alert('стульев не хватает');
+//}
+
+
+
+
